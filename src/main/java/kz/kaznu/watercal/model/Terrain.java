55a8b89d076de0a333756088c76da49a878120be
magzhan.karasayev <fallen.s4e@gmail.com>
2018-09-28 22:58:26 +0600

    Upd: first working version introduced

--- src/main/java/kz/kaznu/watercal/model/Terrain.java
+++ src/main/java/kz/kaznu/watercal/model/Terrain.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -11,10 +12,66 @@ import java.util.stream.Stream;
  * @since 9/28/18 10:08 PM
  */
 public class Terrain {
-    private List<Long> asList;
+    private final List<Long> asList;
+    private final List<Long> highestLeft;
+    private final List<Long> highestRight;
 
     public Terrain(String ... data) {
         this.asList = generateFromStringArray(data);
+        this.highestLeft = initHighestLeft();
+        this.highestRight = initHighestRight();
+    }
+
+    private List<Long> initHighestLeft() {
+        ArrayList<Long> res = new ArrayList<>();
+        Long maxHighestSoFar = 0L;
+        for (Long height : asList) {
+            res.add(maxHighestSoFar);
+            maxHighestSoFar = Math.max(maxHighestSoFar, height);
+        }
+        return res;
+    }
+
+    public Long calc() {
+        return IntStream.range(0, asList.size())
+                .mapToLong(this::findUnitsByIndex)
+                .sum();
+    }
+
+    public long findUnitsByIndex(int index) {
+        Long left = findHighestFromTheLeft(index);
+        System.out.println("left = " + left);
+
+        Long right = findHighestFromTheRight(index);
+        System.out.println("right = " + right);
+
+        long waterLevel = Math.min(left, right);
+        System.out.println("waterLevel = " + waterLevel);
+
+        long unitsOfWater = waterLevel - asList.get(index);
+        System.out.println("unitsOfWater = " + unitsOfWater);
+
+        return Math.max(0, unitsOfWater);
+    }
+
+    private List<Long> initHighestRight() {
+        ArrayList<Long> res = new ArrayList<>(asList.size());
+        asList.forEach(res::add);// FIXME init it correctly
+        Long maxHighestSoFar = 0L;
+        for (int i = asList.size() - 1; i >= 0; i--) {
+            Long height = asList.get(i);
+            res.set(i, maxHighestSoFar);
+            maxHighestSoFar = Math.max(maxHighestSoFar, height);
+        }
+        return res;
+    }
+
+    Long findHighestFromTheLeft(int index) {
+        return highestLeft.get(index);
+    }
+
+    Long findHighestFromTheRight(int index) {
+        return highestRight.get(index);
     }
 
     private List<Long> generateFromStringArray(String ... data) {
--- src/test/java/kz/kaznu/watercal/model/TerrainTest.java
+++ src/test/java/kz/kaznu/watercal/model/TerrainTest.java
@@ -4,24 +4,103 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * @author <a href="mailto:mark.jay.mk@gmail.com">mark jay</a>
  * @since 9/28/18 10:10 PM
  */
 public class TerrainTest {
-    
-    @Test
-    public void terrainConstructor_whenCalled_shouldGenerateCorrectTerrain() {
-        Terrain terrain = new Terrain( "" +
+
+    private Terrain getTerrain() {
+        return new Terrain("" +
                 "      #     # ",
                 "  #   #     # ",
                 "# #   #  #  # ",
                 "# #   #  #  # "
         );
-        Assert.assertEquals(Arrays.<Long>asList(2L, 0L, 3L, 0L, 0L, 0L, 4L, 0L, 0L, 2L, 0L, 0L, 4L, 0L), terrain.getAsList());
-
     }
 
+    @Test
+    public void testConstructor_whenCalled_shouldGenerateCorrectTerrain() {
+        Terrain terrain = getTerrain();
+
+        Assert.assertEquals(Arrays.<Long>asList(2L, 0L, 3L, 0L, 0L, 0L, 4L, 0L, 0L, 2L, 0L, 0L, 4L, 0L), terrain.getAsList());
+    }
+
+    @Test
+    public void testFindHighestFromTheLeft_whenCalled_shouldCalc() {
+        Terrain terrain = getTerrain();
+        testLeft(terrain, 0, 0L);
+        testLeft(terrain, 1, 2L);
+        testLeft(terrain, 2, 2L);
+        testLeft(terrain, 3, 3L);
+        testLeft(terrain, 4, 3L);
+        testLeft(terrain, 5, 3L);
+        testLeft(terrain, 6, 3L);
+        testLeft(terrain, 7, 4L);
+        testLeft(terrain, 8, 4L);
+        testLeft(terrain, 9, 4L);
+        testLeft(terrain, 10, 4L);
+        testLeft(terrain, 11, 4L);
+        testLeft(terrain, 12, 4L);
+    }
+
+    @Test
+    public void testFindHighestFromTheRight_whenCalled_shouldCalc() {
+        Terrain terrain = getTerrain();
+        testRight(terrain, 0, 4L);
+        testRight(terrain, 1, 4L);
+        testRight(terrain, 2, 4L);
+        testRight(terrain, 3, 4L);
+        testRight(terrain, 4, 4L);
+        testRight(terrain, 5, 4L);
+        testRight(terrain, 6, 4L);
+        testRight(terrain, 7, 4L);
+        testRight(terrain, 8, 4L);
+        testRight(terrain, 9, 4L);
+        testRight(terrain, 10, 4L);
+        testRight(terrain, 11, 4L);
+        testRight(terrain, 12, 0L);
+        testRight(terrain, 13, 0L);
+    }
+
+    @Test
+    public void testFindUnitsByIndex_whenCalled_shouldPass() {
+        Terrain terrain = getTerrain();
+        assertHeight(terrain, 0, 0);
+        assertHeight(terrain, 1, 2);
+        assertHeight(terrain, 2, 0);
+        assertHeight(terrain, 3, 3);
+        assertHeight(terrain, 4, 3);
+        assertHeight(terrain, 5, 3);
+        assertHeight(terrain, 6, 0);
+        assertHeight(terrain, 7, 4);
+        assertHeight(terrain, 8, 4);
+        assertHeight(terrain, 9, 2);
+        assertHeight(terrain, 10, 4);
+        assertHeight(terrain, 11, 4);
+        assertHeight(terrain, 12, 0);
+        assertHeight(terrain, 13, 0);
+    }
+
+    @Test
+    public void testCalc_whenCalled_shouldCount() {
+        Terrain terrain = getTerrain();
+        long result = terrain.calc();
+        Assert.assertEquals(
+                0 + 2 + 0 + 3 + 3 + 3 + 0 + 4 + 4 + 2 + 4 + 4 + 0 + 0
+                , result);
+    }
+
+    private void testLeft(Terrain terrain, int index, long expected) {
+        Assert.assertEquals(expected, (long) terrain.findHighestFromTheLeft(index));
+    }
+
+    private void testRight(Terrain terrain, int index, long expected) {
+        Assert.assertEquals("index = " + index, expected, (long) terrain.findHighestFromTheRight(index));
+    }
+
+    private void assertHeight(Terrain terrain, int index, int expected) {
+        Assert.assertEquals(expected, terrain.findUnitsByIndex(index));
+    }
 }
